a95d53d7c014a571700c185bed9da27ba1aea779
EtnexCasto <bro04280@gmail.com>
2025-05-05 00:53:39 +0500

    Подкорректировал нахождение кнопки

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_find_button_add_to_basket(browser):
     browser.get(link)
     try:
-        button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     except NoSuchElementException:
         assert False, "Кнопка добавления в корзину НЕ найдена"
 
